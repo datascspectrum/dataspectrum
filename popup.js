5e3d62d776b0d9b9b79046cb55ea8e3e047564be
jorgealmenta <datasciencespectrum@gmail.com>
2022-10-19 13:36:06 -0400

    correction

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 const formulario = document.getElementById('formulario');
-				formulario.addEventListener('submit', validarForm);				
-				function validarForm(e) {
-					e.preventDefault();
-					const name = document.getElementById('name').value;
-					const email = document.getElementById('email').value;
-					const message = document.getElementById('message').value;
-					if (name === '' || email === '' || message === '') {
-						const msg = document.getElementById('msg');
-						msg.textContent = 'All fields are required';
-					} else {
-						formulario.submit();
-						formulario.reset();                       
-						alert('Thanks for contact me, I would reply as soon as possible');
-					}
-				}
+formulario.addEventListener('submit', validarForm);				
+function validarForm(e) {
+	e.preventDefault();
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const message = document.getElementById('message').value;
+	if (name === '' || email === '' || message === '') {
+		const msg = document.getElementById('msg');
+		msg.textContent = 'All fields are required';
+	} else {
+		formulario.submit();
+		formulario.reset();                       
+		alert('Thanks for contact me, I would reply as soon as possible');
+	}
+}
